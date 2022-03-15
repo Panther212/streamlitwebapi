1c391c80d97eaa9ffdddc26743190e40b8b79efa
Mukund666 <mukund.choudhary14@gmail.com>
2022-03-15 15:55:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-from scipy.fft import fftshift
+from scipy.fft import fftshift,fft, fftfreq, rfft, rfftfreq
 import os
 import glob
 import json
 from PIL import Image
 import csv
+import pymysql
+import boto3
 import json
+
 
 # s3_client = boto3.client('s3')
 # response=s3_client.get_object(Bucket='trebirth1',Key='FB.json')
@@ -20,17 +23,16 @@ import json
 #retrieve json file from firebase
 firebase = firebase.FirebaseApplication('https://esp32-d544d-default-rtdb.firebaseio.com/',None)
 result = firebase.get("test","sensor")
-scan = firebase.get("test","scan_number")
-
-Np_result=np.array(result)
-print(Np_result)
+result1 = firebase.get("test","scan_number")
+Np_result = np.array(result)
 df = pd.DataFrame(Np_result)
-df.to_csv("scan.csv")
+df.to_csv( "scan.csv")
+
 
 
 jtopy=json.dumps(result)       #json.dumps take a dictionary as input and returns a string as output.
 dict_json=json.loads(jtopy)    # json.loads take a string as input and returns a dictionary as output.
-#print(dict_json)
+# print(dict_json)
 
 # Digital Filter starts from here
 def Data_Preprocess(x):
@@ -39,7 +41,7 @@ def Data_Preprocess(x):
  return sig
 
 def Apply_Filter(sig):
-    sos = signal.butter(1, [0.1, 20], 'band', fs=100, output='sos')
+    sos = signal.butter(1, [0.1,10], 'band', fs=100, output='sos')
     filtered = signal.sosfilt(sos, sig)
     # print ("Filtered data is ",filtered)
     return filtered.squeeze()
@@ -51,6 +53,23 @@ def Plot_Graph(filtered):
    fig, ax = plt.subplots()
    x = t.squeeze()
    line, = ax.plot(x, filtered.squeeze())
+   
+def Calculate_FFT(sig_data):
+   N = 1500
+   yf = rfft(sig_data[:1500])
+   xf = rfftfreq(N, 0.01)
+   fig, ax = plt.subplots()
+   ax.plot(xf,np.abs(yf))
+   plt.axis([0, 10, 0, 100000])
+   plt.suptitle('Fourier Transform of the Signal')
+   plt.xlabel("Frequency(Hz)")
+   plt.ylabel("Amplitude")
+   st.pyplot(fig)
+   
+
+   #plt.plot(xf, yf)
+   #plt.show()
+
 
    #plt.plot(t.squeeze(),filtered.squeeze())
   # plt.suptitle('Filtered Scan Data')
@@ -62,7 +81,7 @@ Data = Data_Preprocess(dict_json)
 Filtered_data = Apply_Filter(Data)
 # print(Filtered_data)
 Plot_Graph(Filtered_data)
-# print(Filtered_data)
+print(Filtered_data)
 # plt.show()
 plt.savefig("output.jpg")
 
@@ -74,54 +93,46 @@ a=st.sidebar.radio('Navigation',['Farm Information','Farmer Data'])
 
 if a == "Farm Information":
  st.header("Welcome to Trebirth Tech Development")
- # form = st.form(key='my_form',clear_on_submit=True)
- # F_name= form.text_input(label='Enter Farmer Name')
- # F_health= form.text_input(label='Enter Farm Health')
- # Number= form.number_input(label='Enter No. of trees scanned')
- # Remark = form.text_area(label='Remark')
- # submit_button = form.form_submit_button(label='Submit')
+#  form = st.form(key='my_form',clear_on_submit=True)
+#  F_name= form.text_input(label='Enter Farmer Name')
+#  F_health= form.text_input(label='Enter Farm Health')
+#  Number= form.number_input(label='Enter No. of trees scanned')
+#  Remark = form.text_area(label='Remark')
+#  submit_button = form.form_submit_button(label='Submit')
 
 
- # st.sidebar.markdown(
- #    f"""
- #     * Farmer name :        {F_name}
- #     * Farm health :        {F_health}
- #     * No of trees scanned: {Number}
- #     * Remark      :        {Remark}
- # """
- #  )
+#  st.sidebar.markdown(
+#     f"""
+#      * Farmer name :        {F_name}
+#      * Farm health :        {F_health}
+#      * No of trees scanned: {Number}
+#      * Remark      :        {Remark}
+#  """
+#   )
 
- st.write("Scan number is", scan)
- st.line_chart(Filtered_data,use_container_width=True)
-
+ st.write("Scan number is ", result1)
+ st.line_chart(Filtered_data)
+ st.write("This is a test message!")
+ Calculate_FFT(Np_result)
+ #st.line_chart(Filtered_data, width=1000, height=0, use_container_width=False)
  st.write(df)
+ 
 
 
-
- # st.write(result)
- # st.write(scan)
-
-
- # st.dataframe(df)
-
-
- # df.to_csv("Trebirth.csv",index=False)
- # st.dataframe(df)
- # if submit_button:
- #     st.write(F_name,F_health,Number,Remark)
- # new_data = {"Farmer_Name": F_name,"Farm_Health": F_health,"Trees_Scanned": int(Number),"Remark": Remark}
- #st.write(new_data)
-
- # df = df._append(new_data,ignore_index=True,sort=False)
- # df.to_csv("Trebirth.csv",index=False)
- # st.dataframe(df)
-
+#  if submit_button:
+#      st.write(F_name,F_health,Number,Remark)
+#  new_data = {"Farmer_Name": F_name,"Farm_Health": F_health,"Trees_Scanned": int(Number),"Remark": Remark}
+#  #st.write(new_data)
+#  df = df._append(new_data,ignore_index=True)
+#  df.to_csv("Trebirth.csv",index=False)
+# st.dataframe(df)
 
 
 @st.cache
 def convert_df(df):
-     return df.to_csv().encode('utf-8')
+ return df.to_csv().encode('utf-8')
 csv = convert_df(df)
+
 st.download_button(
      "Press to Download",
      csv,
@@ -143,9 +154,3 @@ st.download_button(
  # with col2:
  #     st.header(" Accelerometer")
  #     st.line_chart(Filtered_data)
-
-
-
-
-
-
